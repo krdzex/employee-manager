282bc167b8ea8b7574f0f73b69d130f57d08f2d6
Krsto Kostic <kostic.krsto@gmail.com>
2021-11-29 23:08:33 +0100

    Finishing project

--- .history/frontend/src/Auth/OnlyAdminRoute_20211129160845.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const OnlyAdminRoute = () => {
-
-    return  authHelper.isAuthentcated().user.role === "Admin" ? <Outlet /> : <Navigate to="/teams" />;
-
-};
-
-export default OnlyAdminRoute;
--- .history/frontend/src/Auth/PrivateRoute_20211123125648.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
-import authHelper from './authHelper';
-
-const PrivateRoute = () => {
-    return authHelper.isAuthentcated() ? <Outlet /> : <Navigate to="/about" />;
-}
-
-export default PrivateRoute
